523b89a9ba20f304cb0db8292c70b7e2fbb5840e
PierreDuv <frozenphenix78@gmail.com>
2023-11-02 10:18:54 +0100

    feat : update tool to latest version

--- LINDRA - Market/LINDRA - Market/Utils/AppSettings.cs
+++ LINDRA - Market/LINDRA - Market/Utils/AppSettings.cs
@@ -9,7 +9,7 @@ namespace LINDRA___Market.Utils
     public static class AppSettings
     {
         public static bool darkMode { get; set; } = false;
-        public static string version { get; set; } = "2.2.3";
-        public static string latestVersion { get; set; } = "2.2.3";
+        public static string version { get; set; } = "2.2.4";
+        public static string latestVersion { get; set; } = "2.2.4";
     }
 }
