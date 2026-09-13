972013cfa7fc2e3201c37330fd8245fff08e91f4
PierroD <frozenphenix78@gmail.com>
2026-09-13 22:27:04 +0200

    fix(MarketOffsets) : update BO2 SP/MP/ZM offsets

--- LINDRA - Market/MarketOffsets/Offsets/t6mp.cs
+++ LINDRA - Market/MarketOffsets/Offsets/t6mp.cs
@@ -7,10 +7,29 @@ using MarketOffsets.Interfaces;
 
 namespace MarketOffsets.Offsets
 {
-    internal class t6mp : IConsole
+    internal class t6mp : IConsole, IFovAndFps, IVisuals, IDisable
     {
-        public long cbuf_addtext => 0x5BDF70;
-        public long nop_address => 0x8C90DA;
+        public long dvar => 0x18;
+        public long cg_fov => 0x10647BC;
+
+        public long cg_fovMin => 0xFCC73C;
+
+        public long cg_fovScale => 0x1062DC0;
+
+        public long com_maxfps => 0x250642C;
+
+        public long r_lightMap => 0x0;
+        public long r_specularMap => 0x34C7238;
+        public long r_filmUseTweaks => 0x34C7060;
+
+        public long cg_brass => 0x0;
+        public long r_fog => 0x34C74EC;
+        public long r_glow => 0x0;
+        public long r_detail => 0x0;
+        public long r_detailMap => 0x0;
+ 
+        public long cbuf_addtext => 0x0; // 0x6BAD90;
+        public long nop_address => 0x0; // 0x8C90DA;
 
     }
 }
--- LINDRA - Market/MarketOffsets/Offsets/t6zm.cs
+++ LINDRA - Market/MarketOffsets/Offsets/t6zm.cs
@@ -10,22 +10,22 @@ namespace MarketOffsets.Offsets
     internal class t6zm : IFovAndFps, IVisuals, IDisable, IConsole
     {
         public long dvar => 0x18;
-        public long cg_fov => 0x111810C;
-        public long cg_fovMin => 0x108008C;
+        public long cg_fov => 0x106284C;
+        public long cg_fovMin => 0xFCA7CC;
 
-        public long cg_fovScale => 0x1116710;
-        public long com_maxfps => 0x25BA4B0;
+        public long cg_fovScale => 0x1060E50;
+        public long com_maxfps => 0x25044AC;
 
         public long r_lightMap => 0x0;
-        public long r_specularMap => 0x342AF10;
-        public long r_filmUseTweaks => 0x342AD38;
+        public long r_specularMap => 0x34C52B8;
+        public long r_filmUseTweaks => 0x34C50E0;
 
         public long cg_brass => 0x0;
-        public long r_fog => 0x342B1C4;
+        public long r_fog => 0x34C556C;
         public long r_glow => 0x0;
         public long r_detail => 0x0;
         public long r_detailMap => 0x0;
-        public long cbuf_addtext => 0x4C7120;
+        public long cbuf_addtext => 0x0; // 6BBE40
         public long nop_address => 0x0;
     }
 }
